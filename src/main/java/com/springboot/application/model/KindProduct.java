8613f040469603410942057c58cd56b4132e055a
tt_pho <tonphosupper@gmail.com>
2021-03-29 13:34:54 +0700

    login_2

---
+++ src/main/java/com/springboot/application/model/KindProduct.java
@@ -0,0 +1,21 @@
+package com.springboot.application.model;
+
+public class KindProduct {
+	private long id_loai;
+	private String name;
+	
+	public long getId_loai() {
+		return id_loai;
+	}
+	public void setId_loai(long id_loai) {
+		this.id_loai = id_loai;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
